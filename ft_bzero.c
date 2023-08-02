79b72ea675189b329dcf6ea6b50466ad12ae1652
Lis Costa <livieira@student.42sp.org.br>
2023-08-02 15:38:28 -0300

    ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,20 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: livieira <livieira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/26 01:28:59 by livieira          #+#    #+#             */
-/*   Updated: 2023/07/27 22:58:38 by livieira         ###   ########.fr       */
+/*   Created: 2023/07/27 22:41:12 by livieira          #+#    #+#             */
+/*   Updated: 2023/07/28 01:57:01 by livieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalpha(int c)
+void	ft_bzero(void *s, size_t n)
 {
-	if (c < 'A' || c > 'Z' && c < 'a' || c > 'z')
-		return (0);
-	return (1);
+	unsigned int	i;
+	char			*c;
+
+	i = 0;
+	c = (char *) s;
+	while (n)
+	{
+		c[i++] = '\0';
+	}
+	return ;
+}
+
+#include <stdio.h>
+int main(void)
+{
+	char *s;
+	
+	s = "A";
+	printf("%s\n", s);
+	printf("%d\n", ft_bzero("A", 1));
+	return (0);
 }
