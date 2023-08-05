2279d12541801c7a42fa944cc14a6cc6f8dc51de
Lis Costa <livieira@student.42sp.org.br>
2023-08-05 05:20:07 -0300

    This function returns a pointer to the last occurrence of character in str. If the value is not found, the function returns a null pointer.

---
+++ ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: livieira <livieira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/05 05:16:08 by livieira          #+#    #+#             */
+/*   Updated: 2023/08/05 05:18:52 by livieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	length;
+
+	length = ft_strlen(s);
+	while (length >= 0)
+	{
+		if (s[length] == (unsigned char) c)
+			return ((char *) s + length);
+		length--;
+	}
+	return (NULL);
+}
