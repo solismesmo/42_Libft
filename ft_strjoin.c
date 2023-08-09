86132f5657614a3d604ae117e3f255c6f2a3a9c9
Lis Costa <livieira@student.42sp.org.br>
2023-08-09 06:35:11 -0300

    remember join???

---
+++ ft_strjoin.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: livieira <livieira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/09 05:55:51 by livieira          #+#    #+#             */
+/*   Updated: 2023/08/09 06:32:06 by livieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	int		i;
+	int		len_s1;
+	int		len_s2;
+
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	join = malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	if (join == NULL)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		join[i] = s1[i];
+	i = -1;
+	while (s2[++i])
+		join[i + len_s1] = s2[i];
+	join[len_s1 + len_s2] = '\0';
+	return (join);
+}
