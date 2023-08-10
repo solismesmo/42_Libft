9f72fe6246d8628590eede416273b7aed5044c8c
Lis Costa <livieira@student.42sp.org.br>
2023-08-10 03:50:26 -0300

    norm check

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: livieira <livieira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 22:38:06 by livieira          #+#    #+#             */
-/*   Updated: 2023/08/10 03:20:58 by livieira         ###   ########.fr       */
+/*   Updated: 2023/08/10 03:49:54 by livieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,21 +33,4 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	}
 	result[i] = '\0';
 	return (result);
-}
-
-
-char my_func(unsigned int i, char str)
- {
- 	printf("My inner function: index = %d and %c\n", i, str);
- 	return str - 32;
- }
-
-
- int main()
- {
- 	char str[10] = "hello.";
- 	printf("The result is %s\n", str);
- 	char *result = ft_strmapi(NULL, NULL);
- 	printf("The result is %s\n", result);
- 	return 0;
 }
