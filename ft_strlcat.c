af7b7d231a2232b40d12ecb47c443d9c2d409aa9
Lis Costa <livieira@student.42sp.org.br>
2023-08-11 04:36:14 -0300

    Fix: strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,38 +6,27 @@
 /*   By: livieira <livieira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 22:41:12 by livieira          #+#    #+#             */
-/*   Updated: 2023/08/05 02:17:38 by livieira         ###   ########.fr       */
+/*   Updated: 2023/08/11 04:13:35 by livieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	char		*dest;
-	const char	*source;
-	size_t		limiter;
-	size_t		dest_len;
+	size_t	x;
+	size_t	y;
 
-	dest = dst;
-	source = src;
-	limiter = size;
-	while (limiter-- != 0 && *dst != '\0')
-		dest++;
-	dest_len = dest - dst;
-	limiter = size - dest_len;
-	if (limiter == 0)
-		return (dest_len + ft_strlen(source));
-	while (*source != '\0')
+	x = 0;
+	y = 0;
+	while (dst[x] && x < size)
+		x++;
+	while (src[y] && x + y + 1 < size)
 	{
-		if (limiter != 1)
-		{
-			*dest++ = *source;
-			limiter--;
-		}
-		source++;
+		dst[x + y] = src[y];
+		y++;
 	}
-	*dest = '\0';
-	return (dest_len + (source - src));
+	if (x != size)
+		dst[x + y] = '\0';
+	return (x + ft_strlen(src));
 }
