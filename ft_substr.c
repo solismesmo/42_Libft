43ace10fcbef85ea7a878265608dd4473bb4ea56
Lis Costa <livieira@student.42sp.org.br>
2023-08-12 15:40:12 -0300

    Update: add if (len_sub - start < len) 		len = len_sub - start;

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: livieira <livieira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 00:04:16 by livieira          #+#    #+#             */
-/*   Updated: 2023/08/11 06:12:48 by livieira         ###   ########.fr       */
+/*   Updated: 2023/08/12 14:10:04 by livieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	unsigned int	i;
 	char			*str;
+	size_t			len_sub;
 
 	if (!s)
 		return (NULL);
-	i = 0;
-	if (start >= ft_strlen(s))
+	len_sub = ft_strlen(s);
+	if (start >= len_sub)
 		return (ft_strdup(""));
+	if (len_sub - start < len)
+		len = len_sub - start;
 	str = malloc(sizeof(char) * len + 1);
 	if (!str)
 		return (NULL);
-	while (i < len)
+	i = 0;
+	while (s[i] && (i < len))
 	{
 		str[i] = s[start + i];
 		i++;
