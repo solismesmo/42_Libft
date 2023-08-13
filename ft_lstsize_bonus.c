a8785baed183543753c9696cad8901e4b73c3609
Lis Costa <livieira@student.42sp.org.br>
2023-08-13 18:03:51 -0300

    WIP: lstsize

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -1,25 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: livieira <livieira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/12 20:01:58 by livieira          #+#    #+#             */
-/*   Updated: 2023/08/13 17:30:57 by livieira         ###   ########.fr       */
+/*   Created: 2023/08/13 17:22:50 by livieira          #+#    #+#             */
+/*   Updated: 2023/08/13 17:23:15 by livieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void *content)
+int			ft_lstsize(t_list *lst)
 {
-	t_list	*new_node;
-
-	new_node = malloc(sizeof(t_list));
-	if (!new_node)
-		return (NULL);
-	new_node->content = content;
-	new_node->next = NULL;
-	return (new_node);
+	
 }
