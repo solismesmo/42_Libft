31af8ff0157c4ef6ab63c22ab2f40993608cb250
Lis Costa <livieira@student.42sp.org.br>
2023-08-13 17:15:46 -0300

    Feat: function lstadd_front to linked a node into a list, in the first node.

---
+++ ft_lstadd_front_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: livieira <livieira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/12 19:45:38 by livieira          #+#    #+#             */
+/*   Updated: 2023/08/13 17:12:10 by livieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!new)
+		return ;
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	new->next = *lst;
+	*lst = new;
+}
