baf4efbc33b2cf0618c28b2c53edba63b954cafd
Lis Costa <livieira@student.42sp.org.br>
2023-08-15 18:13:57 -0300

    Feat: lstadd_back adds the node ’new’ at the end of the list.

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: livieira <livieira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 18:07:14 by livieira          #+#    #+#             */
+/*   Updated: 2023/08/15 18:12:45 by livieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	ft_lstlast(*lst)-> next = new;
+}
