bb1d2bec28251359722d14348ddc32bbab59e67c
Lis Costa <livieira@student.42sp.org.br>
2023-08-15 17:48:28 -0300

    Feat: lstsize for get lenght of list

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -6,13 +6,21 @@
 /*   By: livieira <livieira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 17:22:50 by livieira          #+#    #+#             */
-/*   Updated: 2023/08/13 17:23:15 by livieira         ###   ########.fr       */
+/*   Updated: 2023/08/15 17:47:38 by livieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int			ft_lstsize(t_list *lst)
+int	ft_lstsize(t_list *lst)
 {
-	
+	int	size;
+
+	size = 0;
+	while (lst)
+	{
+		lst = lst -> next;
+		size++;
+	}
+	return (size);
 }
